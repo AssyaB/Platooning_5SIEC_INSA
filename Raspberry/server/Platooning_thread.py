0b144a352a7bcf64915de71ccce5134e51e19d63
BaptisteDelga <36883447+BaptisteDelga@users.noreply.github.com>
2018-11-23 18:28:32 +0100

    Platooning_thread
    Création thread platooning 
    Tentative connection avec raspi de la voiture de devant

---
+++ Raspberry/server/Platooning_thread.py
@@ -0,0 +1,41 @@
+# coding: utf-8
+from threading import Thread
+import time
+import can
+import os
+import struct
+
+HOST = ''# Symbolic name meaning all available interfaces
+HOSTPLAT = "10.105.1.85"
+PORT = 6666              # Arbitrary non-privileged port
+PORTPLAT = 7777
+
+
+class MyPlatooning(Thread):
+
+    def __init__(self,conn, bus):
+        Thread.__init__(self)
+        self.conn = conn
+        self.bus = bus
+
+    def run(self):
+        while True :
+
+            try:
+                bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
+            except OSError:
+                print('Cannot find PiCAN board.')
+                exit()
+
+            splat = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            splat.connect(HOSTPLAT, PORTPLAT)
+            
+            print('Connected by', addr)
+            
+            newthread_platoon = MyReceive(conn, bus)
+            newthread_platoon.start()
+            newsend_platoon = MySend(conn, bus)
+            newsend_platoon.start()
+
+            newthread_platoon.join()
+            newsend_platoon.join()
