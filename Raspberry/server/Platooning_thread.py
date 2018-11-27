4b1522174167ea7753e722f8cd92e3aac422d94e
BaptisteDelga <36883447+BaptisteDelga@users.noreply.github.com>
2018-11-27 09:48:36 +0100

    modif + ajout thread MyReceivePlat

--- Raspberry/server/Platooning_thread.py
+++ Raspberry/server/Platooning_thread.py
@@ -5,37 +5,42 @@ import can
 import os
 import struct
 
-HOST = ''# Symbolic name meaning all available interfaces
 HOSTPLAT = "10.105.1.85"
-PORT = 6666              # Arbitrary non-privileged port
 PORTPLAT = 7777
+
+class MyReceivePlat(Thread):
+    
+    def __init__(self,connplat, bus):
+        Thread.__init__(self)
+        self.bus = bus
+        self.conn = connplat
+        
+    def run(self):
+        while True :
+            data = self.conn.recv(1024)
+            
+            if not data: break  
+                
+            print('Received', repr(data))
+        
+        self.conn.close()        
+
 
 
 class MyPlatooning(Thread):
 
-    def __init__(self,conn, bus):
+    def __init__(self,connplat, bus):
         Thread.__init__(self)
-        self.conn = conn
         self.bus = bus
 
     def run(self):
         while True :
 
-            try:
-                bus = can.interface.Bus(channel='can0', bustype='socketcan_native')
-            except OSError:
-                print('Cannot find PiCAN board.')
-                exit()
-
             splat = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            splat.connect(HOSTPLAT, PORTPLAT)
+            connplat = splat.connect(HOSTPLAT, PORTPLAT)
             
-            newthread_platoon = MyReceive(conn, bus)
+            newthread_platoon = MyReceivePlat(connplat, bus)
             newthread_platoon.start()
-            newsend_platoon = MySend(conn, bus)
-            newsend_platoon.start()
 
             newthread_platoon.join()
-            newsend_platoon.join()
-
-            print('Received', repr(data))
+      
