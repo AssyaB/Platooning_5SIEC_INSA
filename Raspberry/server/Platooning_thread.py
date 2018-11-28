f8bae0f7c9b5ab2c87aa99adea75a1be21a0d4cc
M. Delga Baptiste <delga@insa-10584>
2018-11-28 13:23:25 +0100

    ajout du try

--- Raspberry/server/Platooning_thread.py
+++ Raspberry/server/Platooning_thread.py
@@ -5,31 +5,37 @@ import can
 import os
 import struct
 
+# Echo server program
+import socket
+
 HOSTPLAT = "10.105.0.53"
 PORTPLAT = 7777
+
+#importing variables linked
+from VarNairobi import *
 
 class MyReceivePlat(Thread):
     
     def __init__(self,connplat, bus):
         Thread.__init__(self)
         self.bus = bus
-        self.conn = connplat
+        self.connplat = connplat
         
     def run(self):
         while True :
-            data = self.conn.recv(1024)
+            data = self.connplat.recv(1024)
             
-            if not data: break  
+            if not data: break
                 
             print('Received', repr(data))
         
-        self.conn.close()        
+        self.connplat.close()
 
 
 
 class MyPlatooning(Thread):
 
-    def __init__(self,connplat, bus):
+    def __init__(self,bus):
         Thread.__init__(self)
         self.bus = bus
 
@@ -37,10 +43,14 @@ class MyPlatooning(Thread):
         while True :
 
             splat = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            connplat = splat.connect(HOSTPLAT, PORTPLAT)
-            
-            newthread_platoon = MyReceivePlat(connplat, bus)
-            newthread_platoon.start()
-
-            newthread_platoon.join()
-      
+            try:
+                connplat = splat.connect((HOSTPLAT, PORTPLAT))
+                
+                newthread_platoon = MyReceivePlat(connplat, self.bus)
+                newthread_platoon.start()
+                
+                print("Génial on est connecté")
+                
+                newthread_platoon.join()
+            except socket.error:
+                print("Cosmic ray detected")
